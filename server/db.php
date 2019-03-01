5373dd9a2f63dab095089fd87ff485150fdcbad7
xvalerijus <valeria-art@mail.com>
2019-03-01 11:11:25 +0100

    Update db.php

--- server/db.php
+++ server/db.php
@@ -1,6 +1,6 @@
 <?php
 
-  $pdo = new PDO('mysql:host=localhost;dbname=myservices', 'lucilla', 'lucilla');
+  $pdo = new PDO('mysql:host=localhost;dbname=your_db', 'your_user', 'your_password');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
  ?>
